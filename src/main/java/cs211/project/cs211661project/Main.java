a650f32b2e6b78c901950e0423ac49cc7ece0829
Nattarika6510450356 <123544519+Nattarika6510450356@users.noreply.github.com>
2023-10-12 18:18:49 +0700

    Delete src/main directory

--- src/main/java/cs211/project/cs211661project/Main.java
+++
@@ -1,8 +0,0 @@
-package cs211.project.cs211661project;
-import cs211.project.cs211661project.HelloApplication;
-
-public class Main {
-    public static void main(String[] args) {
-        HelloApplication.main(args);
-    }
-}
--- src/main/java/cs211/project/services/EventDatasource.java
+++
@@ -1,8 +0,0 @@
-package cs211.project.services;
-
-public interface EventDatasource<T> {
-    T readData();
-    void writeData(T data);
-    void updateEventName(String oldEventName, String newEventNameText);
-
-}
